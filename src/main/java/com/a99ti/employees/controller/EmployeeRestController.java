2c00ade8e1fb7c23bbef5131d099bf0a6b4ae407
Attiq <attiqbashir0@gmail.com>
2025-12-27 09:01:48 +0000

    implemented employee ID, PUT, POST, DELETE Endpoints

--- src/main/java/com/a99ti/employees/controller/EmployeeRestController.java
+++ src/main/java/com/a99ti/employees/controller/EmployeeRestController.java
@@ -2,9 +2,11 @@ package com.a99ti.employees.controller;
 
 import com.a99ti.employees.dao.EmployeeDAO;
 import com.a99ti.employees.entity.Employee;
+import com.a99ti.employees.request.EmployeeRequest;
 import com.a99ti.employees.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,12 +33,35 @@ public class EmployeeRestController {
     public List<Employee> findAll(){
         return employeeService.findAll();
     }
-    @Operation(summary = "Fetch single employee", description = "Get a single employee from database")
+    @Operation(summary = "Fetch single employee", description = "Get a single employee from DB")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{employeeId}")
     public Employee getEmployee(@PathVariable @Min(value = 1) long employeeId){
         Employee theEmployee = employeeService.findById(employeeId);
         return theEmployee;
+    }
+
+    @Operation(summary = "Create a new employee", description = "Add a new employee to the DB")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Employee addEmployee(@Valid @RequestBody EmployeeRequest theEmployee){
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @Operation(summary = "Update an employee", description = "Update an existing employee in the DB")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{employeeId}")
+    public Employee updateEmployee(@PathVariable @Min(value = 1) long employeeId, @Valid @RequestBody EmployeeRequest theEmployee){
+        Employee dbEmployee = employeeService.update(employeeId, theEmployee);
+        return dbEmployee;
+    }
+
+    @Operation(summary = "Delete an employee", description = "Delete an employee from the DB")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{employeeId}")
+    public void deleteEmployee(@PathVariable @Min(value = 1) long employeeId){
+        employeeService.deleteById(employeeId);
     }
 
 
