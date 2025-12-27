e582a029ceee68ed61ae19759290a4747ee2f021
Attiq <attiqbashir0@gmail.com>
2025-12-27 08:20:44 +0000

    enhanced employee service with more endpoints

--- src/main/java/com/a99ti/employees/service/EmployeeService.java
+++ src/main/java/com/a99ti/employees/service/EmployeeService.java
@@ -1,10 +1,21 @@
 package com.a99ti.employees.service;
 
 import com.a99ti.employees.entity.Employee;
+import com.a99ti.employees.request.EmployeeRequest;
 
 import java.util.List;
 
 public interface EmployeeService {
 
     List<Employee> findAll();
+
+    Employee findById(long theId);
+
+    Employee save(EmployeeRequest employeeRequest);
+
+    Employee update(long id, EmployeeRequest employeeRequest);
+
+    Employee convertToEmployee(long id, EmployeeRequest employeeRequest);
+
+    void deleteById(long theId);
 }
