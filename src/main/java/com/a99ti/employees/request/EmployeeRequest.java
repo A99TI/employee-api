e4efa59f309b3044806fe095ca1a47a76097c60c
Attiq <attiqbashir0@gmail.com>
2025-12-27 08:27:53 +0000

    added validation to employeeReqeust

--- src/main/java/com/a99ti/employees/request/EmployeeRequest.java
+++ src/main/java/com/a99ti/employees/request/EmployeeRequest.java
@@ -1,11 +1,21 @@
 package com.a99ti.employees.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class EmployeeRequest {
 
+    @NotBlank(message = "First Name is mandatory")
+    @Size(min=2, max = 50, message = "First name must be between 2 and 50 characters")
     private String firstName;
 
+    @NotBlank(message = "Last Name is mandatory")
+    @Size(min=2, max = 50, message = "Last name must be between 2 and 50 characters")
     private String lastName;
 
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Please enter a valid email")
     private String email;
 
     public EmployeeRequest(String firstName, String lastName, String email) {
